6356dca93b472349f1322ff46b1322cfa5812cbc
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-12-09 09:05:25 +0100

    ✨ feat(data processing): add segmentation goal

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,35 @@
-import cv2
+from cv2 import cv2
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from cv2.cv2 import mean
 
 from data import Data
+
+matplotlib.use('TkAgg')
+
+
+def _max_area(num_labels, stats, number):
+    area_list = []
+    for i in range(num_labels):
+        area_list.append(stats[i][4])
+    area_list.sort()
+    if number == 1:
+        try:
+            return 1, area_list[-2], -1
+        except IndexError:
+            return 1, area_list[-1], -1
+    elif number == 2:
+        try:
+            if area_list[-2] - area_list[-3] > 600:
+                return 3, area_list[-2], -1
+            else:
+                return 2, area_list[-2], area_list[-3]
+        except IndexError:
+            if area_list[-1] - area_list[-2] > 600:
+                return 3, area_list[-1], -1
+            else:
+                return 2, area_list[-1], area_list[-2]
 
 
 class DataProcessing:
@@ -22,12 +49,13 @@ class DataProcessing:
             plt.imshow(self.image.working_image, cmap='gray')
             plt.show()
 
-    def _gray_scale(self):
+    def _gray_scale(self, threshold):
         """
         Convert the image to gray scale.
         """
+        self.image.working_image = cv2.cvtColor(self.image.working_image, cv2.COLOR_RGB2BGR)
         gray = cv2.cvtColor(self.image.working_image, cv2.COLOR_BGR2GRAY)
-        ret, self.image.working_image = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+        ret, self.image.working_image = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
         self.show_image()
 
     def _median(self):
@@ -49,7 +77,7 @@ class DataProcessing:
         """
         Apply dilation to the image with a kernel (2,2).
         """
-        kernel = np.ones((2, 2), np.uint8)
+        kernel = np.ones((5, 5), np.uint8)
         self.image.working_image = cv2.dilate(self.image.working_image, kernel)
         self.show_image()
 
@@ -60,36 +88,96 @@ class DataProcessing:
         self.image.working_image = cv2.Canny(self.image.working_image, 100, 200)
         self.show_image()
 
-    def _max_area(self, num_labels, stats):
-        list = []
-        for i in range(num_labels):
-            list.append(stats[i][4])
-        list.sort()
-        try:
-            return list[-2]
-        except IndexError:
-            return list[-1]
-
-    def _max_area_components(self):
+    def _max_area_components(self, number):
         output = cv2.connectedComponentsWithStats(self.image.working_image)
         num_labels = output[0]
         labels = output[1]
         stats = output[2]
-        area = self._max_area(num_labels, stats)
-        for label in range(1, num_labels):
-            if stats[label][4] == area:
-                mask = np.array(labels, dtype=np.uint8)
-                mask[labels == label] = 255
-                # [intX, intY, intW, intH] = cv2.boundingRect(mask)
-                # print(intX, intY, intW, intH)
-                # cv2.rectangle(mask,
-                #               (intX, intY),  # upper left corner
-                #               (intX + intW, intY + intH),  # lower right corner
-                #               255,  # red
-                #               0)
-                self.image.working_image = mask
-                self.show_image()
-                return;
+        number, area1, area2 = _max_area(num_labels, stats, number)
+        if number == 1 or number == 3:
+            for label in range(1, num_labels):
+                if stats[label][4] == area1:
+                    mask = np.array(labels, dtype=np.uint8)
+                    mask[labels == label] = 255
+                    self.image.working_image = mask
+                    self.show_image()
+                    return number
+        if number == 2:
+            mask1, mask2 = None, None
+            for label in range(1, num_labels):
+                if stats[label][4] == area1:
+                    mask1 = np.array(labels, dtype=np.uint8)
+                    mask1[labels == label] = 255
+                if stats[label][4] == area2:
+                    mask2 = np.array(labels, dtype=np.uint8)
+                    mask2[labels == label] = 255
+            self.image.working_image = cv2.bitwise_or(mask1, mask2)
+            self.show_image()
+            return number
+
+    def _line_detection(self):
+        """
+        Detect vertical lines
+        """
+        y_position = 0
+        for line in range(self.image.working_image.shape[0]):
+            if mean(self.image.working_image[line])[0] >= 2 * 255 / self.image.working_image.shape[1]:
+                return line
+        return y_position
+
+    def _column_detection(self, y_position):
+        x_left, x_right = 0, 0
+        for column in range(self.image.working_image.shape[1] - 1):
+            if self.image.working_image[y_position][column] == 255 \
+                    and self.image.working_image[y_position][column + 1] == 255:
+                x_left = column
+                break
+        for column in range(self.image.working_image.shape[1] - 1, 0, -1):
+            if self.image.working_image[y_position][column] == 255 \
+                    and self.image.working_image[y_position][column - 1] == 255:
+                x_right = column
+                break
+        return x_left, x_right
+
+    def _reconstruct_one(self, number):
+        ret, self.image.working_image = cv2.threshold(self.image.working_image, 240, 255, cv2.THRESH_BINARY)
+        y_position = self._line_detection()
+        x_left, x_right = self._column_detection(y_position)
+        x_position = int((x_left + x_right) / 2)
+        if number == 1:
+            cv2.line(self.image.working_image, self.image.base_goal_1, (x_position, y_position), 255, 2)
+        elif number == 2:
+            cv2.line(self.image.working_image, self.image.base_goal_2, (x_position, y_position), 255, 2)
+
+    def _reconstruct_two(self):
+        # Cut the image in two
+        sub_image_1 = self.image.working_image[0: self.image.working_image.shape[0],
+                      0: self.image.working_image.shape[1] // 2]
+        sub_image_2 = self.image.working_image[0: self.image.working_image.shape[0],
+                      self.image.working_image.shape[1] // 2: self.image.working_image.shape[1]]
+        self.image.working_image = sub_image_1
+        self._reconstruct_one(1)
+        sub_image_1 = self.image.working_image
+        self.image.working_image = sub_image_2
+        self.image.base_goal_2 = (self.image.base_goal_2[0] - self.image.width // 2, self.image.base_goal_2[1])
+        self._reconstruct_one(2)
+        sub_image_2 = self.image.working_image
+        self.image.base_goal_2 = (self.image.base_goal_2[0] + self.image.width // 2, self.image.base_goal_2[1])
+        self.image.working_image = np.zeros((self.image.height, self.image.width))
+        self.image.working_image[0: sub_image_1.shape[0], 0: sub_image_1.shape[1]] = sub_image_1
+        self.image.working_image[0: sub_image_2.shape[0],
+        sub_image_1.shape[1]: sub_image_1.shape[1] + sub_image_2.shape[1]] = sub_image_2
+        self.show_image()
+
+    def _reconstruct_goal(self):
+        """
+        Reconstruct the goal position.
+        """
+        self._reconstruct_two()
+        y_position = self._line_detection()
+        x_left, x_right = self._column_detection(y_position)
+        cv2.line(self.image.working_image, (self.image.base_goal_1[0], y_position),
+                 (self.image.base_goal_2[0], y_position), 255, 2)
 
     def _superpose(self):
         """
@@ -100,15 +188,35 @@ class DataProcessing:
         new_mask = cv2.cvtColor(new_mask, cv2.COLOR_GRAY2BGR)
         new_mask[mask >= 180] = [0, 255, 0]
         self.image.working_image = cv2.addWeighted(self.image.base_image, 0.5, new_mask, 0.5, 0)
+        self.image.working_image = cv2.addWeighted(self.image.working_image, 0.5, self.image.predict_image, 0.5, 0)
         self.show_image()
 
-    def segmentation_goal(self):
-        self._gray_scale()
+    def segmentation_post(self):
+        """
+        Segment one post
+        """
+        self._gray_scale(130)
         self._erode()
         self._erode()
         self._median()
-        # self._dilate()
         self._contours()
-        self._max_area_components()
+        self._max_area_components(1)
         self._dilate()
+        self._reconstruct_one(1)
+        self._superpose()
+
+    def segmentation_goal(self):
+        """
+        Segment the goal.
+        """
+        self._gray_scale(175)
+        self._erode()
+        self._median()
+        self._contours()
+        number = self._max_area_components(2)
+        self._dilate()
+        if number == 2:
+            self._reconstruct_two()
+        elif number == 3:
+            self._reconstruct_goal()
         self._superpose()
