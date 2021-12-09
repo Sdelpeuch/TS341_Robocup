5c8f61d69486e4ff748773672f31c414c6251e04
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-12-09 09:07:09 +0100

    ✨ feat(pipeline): update pipeline

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 
 import cv2
-from data import Data
+import matplotlib.pyplot as plt
+
+from DeepLearningProcessing import DeepLearningProcessing
+from Process import Process
 from data_processing import DataProcessing
+
 
 def folder_process(folder):
     """
@@ -11,16 +15,42 @@ def folder_process(folder):
     for filename in os.listdir(folder):
         try:
             print(filename)
-            data = DataProcessing(folder, filename)
-            data.segmentation_goal()
+            data = DataProcessing(folder, filename, False)
+            deepLearning = DeepLearningProcessing(data.image)
+            deepLearning.predict((150, 300), (-1, -1))
+            process = Process(data)
+            process.process()
             data.image.save()
         except cv2.error:
             pass
         except AttributeError:
             pass
 
+
+def video(folder):
+    """
+    Create a video from a folder of png
+    """
+    images = []
+    for filename in os.listdir(folder):
+        images.append(cv2.imread(folder + filename))
+    height, width, layers = images[0].shape
+    video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, (width, height))
+    for image in images:
+        video.write(image)
+    cv2.destroyAllWindows()
+    video.release()
+
+
 if __name__ == '__main__':
-    data = DataProcessing("data/", "test.png", True)
-    data.segmentation_goal()
+    data = DataProcessing("data/", "test.png", False)
+    deepLearning = DeepLearningProcessing(data)
+    deepLearning.predict()
+    print(data.image.base_goal_1, data.image.base_goal_2)
+    process = Process(data)
+    process.process()
+    plt.imshow(data.image.working_image)
+    plt.show()
     data.image.save()
     # folder_process("data/")
+    # video("process/")
